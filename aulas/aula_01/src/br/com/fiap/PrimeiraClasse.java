bcb1a16133d31c1078cfa0d6fa6484231ead1a26
Rafael Oliveira <162996342+yRaffa@users.noreply.github.com>
2025-02-14 09:06:16 -0300

    update

---
+++ aulas/aula_01/src/br/com/fiap/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+// Diagrama de classes: https://app.diagrams.net/
+
+package br.com.fiap;
+
+public class PrimeiraClasse {
+    // main
+    public static void main(String[] args) {
+        // sout ("" - string | '' - char)
+        System.out.println("Hello World!");
+        System.out.println('A');
+    }
+}
