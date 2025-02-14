addd45c0ccc806509e7d1f31633f9cb3eafa390d
Rafael Oliveira <162996342+yRaffa@users.noreply.github.com>
2025-02-14 09:41:34 -0300

    update

--- aulas/aula_01/src/br/com/fiap/PrimeiraClasse.java
+++ aulas/aula_01/src/br/com/fiap/PrimeiraClasse.java
@@ -1,5 +1,13 @@
 // Diagrama de classes: https://app.diagrams.net/
 
+// Polimorfismo:
+
+// Sobrescrita: Sobrescreve métodos de mesmo nome das classes 'Pai' na herança.
+
+// Sobrcarga: Diversos metodos de mesmo nome, mas com 'assinaturas' diferentes
+// exibir(texto) -> exibir("Hello World!")
+// exibir(texto, numero) -> exibir("Ola Mundo", 33)
+
 package br.com.fiap;
 
 public class PrimeiraClasse {
