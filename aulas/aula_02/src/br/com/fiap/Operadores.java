93aa40cee3795afb4d817aefba40ea156f2ff355
Rafael Oliveira <yraffa.olive@gmail.com>
2025-02-18 11:48:41 -0300

    Update Operadores.java

--- aulas/aula_02/src/br/com/fiap/Operadores.java
+++ aulas/aula_02/src/br/com/fiap/Operadores.java
@@ -2,12 +2,12 @@ package br.com.fiap;
 
 public class Operadores {
     public static void main(String[] args) {
-        byte xByte = 1, yByte = 2, resultadoByte = 0;
-        short xShort = 10, yShort = 20, resultadoShort = 0;
-        int xInt = 100, yInt = 200, resultadoInt = 0;
-        long xLong = 1000, yLong = 2000, resultadoLong = 0;
-        float xFloat = 10.1f, yFloat = 20.2f, resultadoFloat = 0;
-        double xDouble = 100.11, yDouble = 200.22, resultadoDouble = 0;
+        byte xByte = 1, yByte = 2, resultadoByte;
+        short xShort = 10, yShort = 20, resultadoShort;
+        int xInt = 100, yInt = 200, resultadoInt;
+        long xLong = 1000, yLong = 2000, resultadoLong;
+        float xFloat = 10.1f, yFloat = 20.2f, resultadoFloat;
+        double xDouble = 100.11, yDouble = 200.22, resultadoDouble;
 
         resultadoByte = (byte) (xByte + yByte);
         resultadoShort = (short) (xShort - yShort);
