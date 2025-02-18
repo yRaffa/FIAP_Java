63c41e0b8b17b2558169f1128eaa2ef159649f29
Rafael Oliveira <yraffa.olive@gmail.com>
2025-02-18 11:34:19 -0300

    aula02

---
+++ aulas/aula_02/src/br/com/fiap/Constante.java
@@ -0,0 +1,12 @@
+package br.com.fiap;
+
+// final antes do tipo: constantes (TUDO_MAIÚSCULO)
+
+public class Constante {
+    public static void main(String[] args) {
+        final float PI = 3.1415926f;
+        final float GRAVIDADE = 9.8f;
+
+        System.out.println("Pi: " + PI + "\nGravidade: " + GRAVIDADE);
+    }
+}
--- aulas/aula_02/src/br/com/fiap/Primitivo.java
+++ aulas/aula_02/src/br/com/fiap/Primitivo.java
@@ -1,4 +1,17 @@
 package br.com.fiap;
 
 public class Primitivo {
+    public static void main(String[] args) {
+        // declaração e iniciação de variáveis
+        char genero = 'M';
+        byte idade = 22;
+        short salario = 1600;
+        int rm = 554736, alunos = 50, salas = 10;
+        long brasileiros = 216535748;
+        float nota = 5.5f; // especificando que é float
+        double dolar = 5.75;
+        boolean sim = true;
+
+        System.out.println("Genero: " + genero + "\nIdade: " + idade + "\nSalario: " + salario + "\nRMS: " + rm + "\t\tAlunos: " + alunos + "\t\tSalas: " + salas + "\nBrasileiros: " + brasileiros + "\nNota: " + nota + "\nDolar: " + dolar + "\nSim: " + sim);
+    }
 }
