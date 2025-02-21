21bed58720f05f197ab453632881a5f39e0ac5c0
Rafael Oliveira <yraffa.olive@gmail.com>
2025-02-21 13:53:11 -0300

    aula03

---
+++ aulas/aula_03/src/br/com/fiap/Ex02Idade.java
@@ -0,0 +1,24 @@
+package br.com.fiap;
+
+import javax.swing.*;
+
+public class Ex02Idade {
+    public static void main(String[] args) {
+        int anoAtual = 0, anoDeNascimento = 0, idade = 0;
+        String aux;
+
+        try {
+            aux = JOptionPane.showInputDialog("Digite o ano atual: ");
+            anoAtual = Integer.parseInt(aux);
+
+            aux = JOptionPane.showInputDialog("Digite seu ano de nascimento: ");
+            anoDeNascimento = Integer.parseInt(aux);
+
+            idade = anoAtual - anoDeNascimento;
+            JOptionPane.showMessageDialog(null, "Idade: " + idade);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "As datas devem ser numeros inteiros!!!");
+        }
+    }
+}
---
+++ aulas/aula_03/src/br/com/fiap/Ex03AreaCirculo.java
@@ -0,0 +1,21 @@
+package br.com.fiap;
+
+import javax.swing.*;
+
+public class Ex03AreaCirculo {
+    public static void main(String[] args) {
+        float raio = 0.0f, area = 0.0f, pi = 3.14f;
+        String aux;
+
+        try {
+            aux = JOptionPane.showInputDialog("Digite o raio do circulo: ");
+            raio = Float.parseFloat(aux);
+
+            area = pi * (raio * raio);
+            JOptionPane.showMessageDialog(null, "Area do circulo: " + area);
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "O raio deve ser um numero!!!");
+        }
+    }
+}
