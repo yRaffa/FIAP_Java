bffe1d99766194473d6c2dede259c8420c3c1f15
Rafael Oliveira <yraffa.olive@gmail.com>
2025-02-25 10:25:35 -0300

    Update Ex01MediaDeNotasConsole.java

--- aulas/aula_03/src/br/com/fiap/Ex01MediaDeNotasConsole.java
+++ aulas/aula_03/src/br/com/fiap/Ex01MediaDeNotasConsole.java
@@ -23,7 +23,6 @@ public class Ex01MediaDeNotasConsole {
             media = (prova01 + prova02 + prova03 + prova04) / 4;
             System.out.println("Nota 01: " + prova01 + "\nNota 02: " + prova02 + "\nNota 03: " + prova03 + "\nNota 04: " + prova04 + "\nMedia = " + media);
 
-
         } catch (Exception e) {
             System.out.println("As notas devem ser numeros!!!");
         }
